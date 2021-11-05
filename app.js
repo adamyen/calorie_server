8e56d592249e9c4cfa4dbd884d9f776e26ddb83f
Adam Yen <adamyen@Adams-MacBook-Pro.local>
2021-11-04 22:18:19 -0400

    [feat]: app rebuild

--- app.js
+++ app.js
@@ -1,20 +1,65 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const userRouter = require('./routers/users.routes')
-
-const URL = 'mongodb://localhost/CalorieServer';
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
+
+const corsOptions = {
+    origin: "http://localhost:3000",
+};
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// routes
+require('./routes/auth.routes')(app);
+require('./routes/user.routes')(app);
+
 app.listen(8000, () => {
     console.log('Server running...')
 });
 
-app.use('/user', userRouter);
+const dbConfig = require('./config/db.config')
+const db = require("./models");
+const Role = db.role;
 
-mongoose.connect(URL, { useNewUrlParser: true });
-const con = mongoose.connection;
+db.mongoose
+    .connect(`mongodb://${dbConfig.HOST}/${dbConfig.DB}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Successfully connect to MongoDB.");
+        initial();
+    })
+    .catch(err => {
+        console.error("Connection error", err);
+        process.exit();
+    });
 
-con.on('open', () => {
-    console.log('Mongodb connected...')
-});
+function initial() {
+    Role.estimatedDocumentCount((err, count) => {
+        if (!err && count === 0) {
+            new Role({ name: "user" }).save(err => {
+                if (err) {
+                console.log("error", err);
+                }
 
+                console.log("added 'user' to roles collection");
+            });
+
+            new Role({ name: "admin" }).save(err => {
+                if (err) {
+                console.log("error", err);
+                }
+
+                console.log("added 'admin' to roles collection");
+            });
+        }
+    });
+}
